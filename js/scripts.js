e084b2a57e78f7eb3f6ef6951ffd986beabd7c90
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-03-29 09:58:28 -0700

    add basic html and begin writing scripts page
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("form#favoritefoods").submit(function (event) {
+    event.preventDefault();
+    const breakfast = 
+    const lunch =
+    const dinner =
+  }
+}

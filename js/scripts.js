7ce785a0a00330ecc7b5869d4eafb26c60af4eb7
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-03-29 11:14:11 -0700

    add last bit of scripts page with final arrays and appends
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,24 @@ $(document).ready(function () {
   $("form#favoritefoods").submit(function (event) {
     event.preventDefault();
     const breakfastInput = $("#breakfast").val();
-    const lunchInput = $("#lunch").val();
+    const lunchInput = $("#lunch").val(); // sandwich
     const dinnerInput = $("#dinner").val();
-    let array =[breakfastInput,lunchInput, dinnerInput]; // array = [0, 1, 2];
-    const lunch = array[1];
+    const array =[breakfastInput,lunchInput /* sandwich */, dinnerInput]; 
+    const lunch = array[1]; // sandwich
     const breakfast = array[0];
     const dinner = array[2];
-    array.push(1,0,2);
-    console.log (lunch,breakfast,dinner);
+    console.log (array);
+    
+    let newArray = []; // need newArray in order of [breakfast, lunch, dinner] using push() 3 times
+    
+    newArray.push(breakfast);
+    newArray.push(lunch);
+    newArray.push(dinner);
+    console.log(newArray);
+
+    $("#newarray").append(`<li>Your fav breakfast food: ${newArray[0]}</li>`);
+    $("#newarray").append(`<li>${newArray[1]}</li>`);
+    $("#newarray").append(`<li>${newArray[2]}</li>`);
 
   });
 }); 

3414c285444820bb6055844fb2a9c06540941e3b
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-03-29 10:32:57 -0700

    add arrays in scripts page
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function () {
   $("form#favoritefoods").submit(function (event) {
     event.preventDefault();
-    const breakfast = 
-    const lunch =
-    const dinner =
-  }
-}
+    const breakfastInput = $("#breakfast").val();
+    const lunchInput = $("#lunch").val();
+    const dinnerInput = $("#dinner").val();
+    let array =[breakfastInput,lunchInput, dinnerInput]; // array = [0, 1, 2];
+    const lunch = array[1];
+    const breakfast = array[0];
+    const dinner = array[2];
+    array.push(1,0,2);
+    console.log (lunch,breakfast,dinner);
+
+  });
+}); 
